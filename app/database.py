e69ef97dde314cdc91cef537880ad0df51272a65
Adefemi Adewusi <adefemiadewusi07@gmail.com>
2026-07-16 17:16:13 +0100

    edit database config

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-engine = create_engine(DATABASE_URL,connect_args={'check_same_thread':False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
 Base = declarative_base()
 
