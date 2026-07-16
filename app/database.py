72e756f868ef28eb5595fb5689e9b968a332b84a
Adefemi Adewusi <adefemiadewusi07@gmail.com>
2026-07-16 17:03:31 +0100

    create and update .env file

--- app/database.py
+++ app/database.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 import app.models as models
 from sqlalchemy import Column,Integer,String,Float,DateTime,ForeignKey
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./test.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv('DATABASE_URL')
 engine = create_engine(DATABASE_URL,connect_args={'check_same_thread':False})
 SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
 Base = declarative_base()
